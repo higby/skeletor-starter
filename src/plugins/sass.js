acd480e3297c25ba08b890f7fd0d14083795e0b5
Branden Higby <branden@higby.io>
2023-07-06 23:15:57 -0700

    Use `@higby/reset`

--- src/plugins/sass.js
+++ src/plugins/sass.js
@@ -7,7 +7,7 @@ module.exports = config => {
   config.addGlobalData('styles', async () => {
     return sass.compileString(fs.readFileSync(`${stylesDirectory}main.scss`, 'utf8'), {
       style: 'compressed',
-      loadPaths: [stylesDirectory]
+      loadPaths: [stylesDirectory, 'node_modules/']
     }).css
   })
 }
